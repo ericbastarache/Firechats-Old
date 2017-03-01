d9de5491b1aeb49d12297b6cb0491296c5411e48
ericb <eric_bastarache@hotmail.com>
2017-03-01 18:49:45 -0500

    Updated header, footer; angular-cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
