1825dcdc0e5af4a90c4d9dd5cf63ed191ff3cc29
ericb <eric_bastarache@hotmail.com>
2017-03-01 18:42:44 -0500

    Created preliminary server entry point

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const http = require('http');
+
+const api = require('./server/routes');
