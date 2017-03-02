ce815d1e428763452f7ffc625209d13900f13ed6
ericb <eric_bastarache@hotmail.com>
2017-03-01 19:17:41 -0500

    Removed server file

--- server.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const http = require('http');
-
-const api = require('./server/routes');
