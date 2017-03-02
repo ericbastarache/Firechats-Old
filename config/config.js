41e0eee475b874d4ff6173d701c0b10a27271a78
ericb <eric_bastarache@hotmail.com>
2017-03-01 19:18:00 -0500

    Updated sequelize config

--- config/config.js
+++ config/config.js
@@ -7,10 +7,6 @@ module.exports = {
     url: process.env.DATABASE_URL,
     dialect: 'postgres'
   },
-  staging: {
-    url: process.env.DATABASE_URL,
-    dialect: 'postgres'
-  },
   test: {
     url: process.env.DATABASE_URL || 'postgres://postgres:password@localhost:5432/bookmark_test',
     dialect: 'postgres'
